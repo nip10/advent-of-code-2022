bbb869b423acf65ad1a0177d8c87f50407e5eff5
Diogo Cardoso <diogocardoso92@outlook.com>
2022-12-04 13:19:00 +0000

    add day4

---
+++ day-04/day04b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	inputBytes, err := os.ReadFile("./input.txt")
+	check(err)
+	inputString := string(inputBytes)
+	inputStringWithoutWindowsNewLines := strings.ReplaceAll(inputString, "\r\n", "\n")
+	pairs := strings.Split(inputStringWithoutWindowsNewLines, "\n")
+
+	result := 0
+	for _, pair := range pairs {
+		pairParts := strings.Split(pair, ",")
+		sections1 := strings.Split(pairParts[0], "-")
+		min1, _ := strconv.Atoi(sections1[0])
+		max1, _ := strconv.Atoi(sections1[1])
+		sections2 := strings.Split(pairParts[1], "-")
+		min2, _ := strconv.Atoi(sections2[0])
+		max2, _ := strconv.Atoi(sections2[1])
+		if (min1 <= min2 && max1 >= min2) || (min1 >= min2 && min1 <= max2) {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
